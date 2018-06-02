2583542ff0cdc2e1a881fbcbffdb2ac42157ae20
Rocha <valentin.rocha@etu.udamail.fr>
2018-06-02 11:56:16 +0200

    Toujours sur la VM

--- AkaProject.RochaValentin/App/ViewModel/Common/RelayCommand.cs
+++ AkaProject.RochaValentin/App/ViewModel/Common/RelayCommand.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace App.ViewModel.Common
 {
-    class RelayCommand
+    public class RelayCommand : ICommand
     {
+
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+        private object p;
+
+        public RelayCommand(Action execute) : this(execute, null)
+        {
+
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentException("execute");
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+       
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (this.canExecute != null)
+                    CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                if (this.canExecute != null)
+                    CommandManager.RequerySuggested -= value;
+            }
+        }
     }
 }
--- AkaProject.RochaValentin/App/ViewModel/DetailsProductVM.cs
+++ AkaProject.RochaValentin/App/ViewModel/DetailsProductVM.cs
@@ -1,13 +1,70 @@
-﻿using System;
+﻿using App.ViewModel.Common;
+using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace App.ViewModel
 {
     public class DetailsProductVM
     {
+        private int _code;
+        private string _description;
+        private int _stock;
+        private int _actif;
+        private RelayCommand _addOperation;
 
+
+        public DetailsProductVM(Product product)
+        {
+            _code = product.Code;
+            _description = product.Description;
+            _stock = product.Stock;
+            _actif = product.Actif;
+        }
+
+        public int Code
+        {
+            get { return _code; }
+            set { _code = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public int Stock
+        {
+            get { return _stock; }
+            set { _stock = value; }
+        }
+
+        public int Actif
+        {
+            get { return _actif; }
+            set { _actif = value; }
+        }
+
+        public ICommand addOperation
+        {
+            get
+            {
+                if (_addOperation == null)
+                    _addOperation = new RelayCommand(() => this.ShowWindowOperation());
+                return _addOperation;
+            }
+        }
+
+        private void ShowWindowOperation()
+        {
+            View.Operation operationWindow = new View.Operation();
+            operationWindow.DataContext = this;
+            operationWindow.ShowDialog();
+        }
     }
 }
